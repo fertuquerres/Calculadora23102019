244de84048d66594b0e133a2bda8181252f1ee58
fertuquerres <56898229+fertuquerres@users.noreply.github.com>
2019-10-23 15:08:53 -0500

    Add files via upload

---
+++ Calculadora.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_Calculadora
+{
+    public class Calculadora
+    {
+        private double num1;
+        private double num2;
+
+        public Calculadora()
+        {
+            num1 = 0;
+            num2 = 0;
+        }
+        public Calculadora(double nnum1, double nnum2)
+        {
+            nnum1 = num1;
+            nnum2 = num2;
+        }
+        public void SetNum1(double nnum1)
+        {
+            num1 = nnum1;
+        }
+        public double GetNum1()
+        {
+            return num1;
+        }
+        public void SetNum2(double nnum2)
+        {
+            num2 = nnum2;
+        }
+        public double GetNum2()
+        {
+            return num2;
+        }
+        public double Suma()
+        {
+            return num1 + num2;
+        }
+        public double Resta()
+        {
+            return num1 - num2;
+        }
+        public double Multiplicacion()
+        {
+            return num1 * num2;
+        }
+        public double Division()
+        {
+            return num1 / num2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora miCalculadora = new Calculadora();
+            Console.Write("Ingrese el primer numero: ");
+            miCalculadora.SetNum1(Convert.ToDouble(Console.ReadLine()));
+            Console.Write("Ingrese el segundo numero: ");
+            miCalculadora.SetNum2(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("La suma es: " + miCalculadora.Suma());
+            Console.WriteLine("La resta es: " + miCalculadora.Resta());
+            Console.WriteLine("La multiplicacion es: " + miCalculadora.Multiplicacion());
+            Console.WriteLine("La division es: " + miCalculadora.Division().ToString("0.00"));
+            Console.ReadKey();
+        }
+    }
+}
